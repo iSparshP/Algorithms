ef5cb77470f4f012140d9faccd6bafb607f59011
Sparsh <77487266+iSparshP@users.noreply.github.com>
2023-12-24 16:36:23 +0530

    Create Bilkul ricks nhi lena ka.cpp

---
+++ Bit Manipulations/Bilkul ricks nhi lena ka.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll int64_t
+#define endl '\n'
+
+void solve() {
+    ll x, y;
+    cin >> x >> y;
+    cout << x + y << endl;
+}
+int main() {
+    ios_base :: sync_with_stdio(0);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int t = 1;
+    // int i = 1;
+    // cin >> t;
+    while (t--) {
+        // cout << "Case #" << i << ": ";
+        solve();
+        // i++;
+    }
+    return 0;
+}
