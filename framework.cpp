2bb74b182c3faa5f170bb18d746a0e5a5d2ee427
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-07-18 12:42:34 +0530

    Create framework.cpp

---
+++ framework.cpp
@@ -0,0 +1,29 @@
+int rec(int par1, int par2)//level for the dp where we going to iterate to find the answer
+{
+    //pruning
+    
+    //base case
+    
+    //cache check
+    
+    //compute / transition
+    
+    //save and return
+    
+}
+
+int backtrack(int part1)//level for backtrack
+{
+    //for(choice)
+    // {
+    //     if(compute)
+    //     {
+    //         move
+    //     }
+    // }
+    
+    //L:level
+    //C:choice
+    //C:check
+    //M:move
+}
