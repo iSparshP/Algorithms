ffd280af66293b0a49331019f5d41dfee5a24c23
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-09-29 03:21:29 +0530

    Time: 8 ms (77.75%), Space: 14.1 MB (47.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        unordered_map<int,int>unMap;
-        for(int i=0;i<n;i++)
-        {
-            int comp=target-nums[i];
-            if(unMap.find(comp)!=unMap.end())
-            {
-                return vector<int>{unMap[comp],i};
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            int compliment=target-nums[i];
+            if(mp.find(compliment)!=mp.end()){
+                //the map is having the compliment value
+                return vector<int>{mp[compliment],i};
             }
-            unMap[nums[i]]=i;
+            mp[nums[i]]=i;
         }
         return {};
     }
