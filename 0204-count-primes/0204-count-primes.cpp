42f9620a7cbc96554ee633650506c5d8b132a735
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-09-29 12:32:05 +0530

    Time: 443 ms (45.27%), Space: 11.8 MB (42.53%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n <= 2) return 0;  // There are no primes less than 2
-        int count = 0;
-        vector<bool> check(n, true);  // Initialize size n
-        check[0] = false;
-        check[1] = false;
-        
-        for (int i = 2; i < n; i++) {
-            if (check[i]) {
+        if(n<=2){
+            return 0;
+        }
+        vector<bool>prime(n,true);
+        prime[0]=false;
+        prime[1]=false;
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(prime[i]){
                 count++;
-                if ((long long)i * i < n) {  // Avoid overflow by checking the range
-                    for (int j = i * i; j < n; j += i) {
-                        check[j] = false;
+                if((long long)i*i<n){
+                    for(int j=i*i;j<n;j+=i){
+                        prime[j]=false;
                     }
                 }
             }
