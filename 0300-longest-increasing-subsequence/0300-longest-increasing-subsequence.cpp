08e0bab0b2a4e9d78d295919f6896e8f8dbb39fb
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-09-30 02:18:47 +0530

    Time: 92 ms (48.47%), Space: 13.3 MB (52.63%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
-    int solve(int level,vector<int>& nums,vector<int>&dp){
-        //pruning
-        if(level<0){
-            return 0;
-        }
-        //base
+    // ((Memoization))
+    // int solve(int level,vector<int>& nums,vector<int>&dp){
+    //     //pruning
+    //     if(level<0){
+    //         return 0;
+    //     }
+    //     //base
 
-        //cache
-        if(dp[level]!=-1){
-            return dp[level];
-        }
-        //save and compute
-        int ans=1;
-        for(int previous=0;previous<level;previous++){
-            if(nums[previous]<nums[level]){
-                ans=max(ans,1+solve(previous,nums,dp));
-            }
-        }
-        //return 
-        return dp[level]=ans;
-    }
+    //     //cache
+    //     if(dp[level]!=-1){
+    //         return dp[level];
+    //     }
+    //     //save and compute
+    //     int ans=1;
+    //     for(int previous=0;previous<level;previous++){
+    //         if(nums[previous]<nums[level]){
+    //             ans=max(ans,1+solve(previous,nums,dp));
+    //         }
+    //     }
+    //     //return 
+    //     return dp[level]=ans;
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        int best=0;
-        for(int i=0;i<n;i++){
-            best=max(best,solve(i,nums,dp));
+        if(n==0){
+            return 0;
         }
-        return best;
+        vector<int>dp(n,1);
+        for(int level=1;level<n;level++){
+            for(int previous=0;previous<level;previous++){
+                if(nums[previous]<nums[level]){
+                    dp[level]=max(dp[level],dp[previous]+1);
+                }
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
     }
 };
