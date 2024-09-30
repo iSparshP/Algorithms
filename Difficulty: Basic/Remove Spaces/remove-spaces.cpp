45de79d8d99c73131024fa663c138cb13c16241a
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-09-30 23:22:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]==' '){
+                continue;
+            }
+            else{
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
