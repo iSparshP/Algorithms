52283e57e4ebf6018f16f5cdd0ee9e5ee0b2d3ec
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-10-04 17:12:28 +0530

    Time: 3 ms (12.71%), Space: 7.3 MB (88.32%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -3,8 +3,8 @@ public:
     int getSum(int a, int b) {
         while(b){
             int carry=a&b;
-            a = a ^ b;
-            b = carry << 1;
+            a=a^b;
+            b=carry<<1;
         }
         return a;
     }
