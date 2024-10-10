e31babdd62944b32a98f83fa5bb025729d869332
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-10-10 11:36:36 +0530

    Time: 53 ms (24.07%), Space: 51.2 MB (18.76%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int target = nums.size() - 1;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(target<=i+nums[i]){
+                target=i;
+            }
+        }
+        return target==0;
+    }
+};
