5903e356dd9d73a80517772aae828061485ef2e9
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-10-10 11:43:42 +0530

    Time: 511 ms (11.14%), Space: 54.3 MB (6.15%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int target = nums.size() - 1;
-        for(int i=nums.size()-2;i>=0;i--){
-            if(target<=i+nums[i]){
-                target=i;
+    bool function(int index, int &n, vector<int>& nums, vector<int>& dp){
+        //base
+        if(index>=n-1){
+            return true;
+        }
+        //cache
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+        //save and compute
+        int maxJump=nums[index];
+        for(int i=1;i<=maxJump;i++){
+            if(function(index+i,n,nums,dp))
+            {
+                return dp[index] = true;
             }
         }
-        return target==0;
+        //return
+        return dp[index]=false;
+    }
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return function(0,n,nums,dp);
     }
 };
