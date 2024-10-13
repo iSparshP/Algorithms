860536cfa9e960c3cc87de15e617c94cc1b424a3
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-10-13 12:12:02 +0530

    Time: 3 ms (80.54%), Space: 16.9 MB (27.74%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -8,34 +8,13 @@ class Solution
             {
                 return n == 0 ? 0 : cost[0];
             }
-
             vector<int> dp(n);
             dp[0] = cost[0];
             dp[1] = cost[1];
-
             for (int i = 2; i < n; i++)
             {
                 dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
             }
-
             return min(dp[n - 1], dp[n - 2]);
         }
-   	// int mincost(vector<int>& cost, int n, vector<int>& dp){
-   	//    	//pruning
-   	//     if(n < 0){
-   	//         return 0;
-   	//     }
-   	//    	//base case
-   	//     if(n==0||n==1){
-   	//         return cost[n];
-   	//     }
-   	//    	//cache
-   	//     if(dp[n]!=-1){
-   	//         return dp[n];
-   	//     }
-   	//    	//save and compute
-   	//     dp[n]=cost[n]+min(mincost(cost,n-1,dp),mincost(cost,n-2,dp));
-   	//    	//return
-   	//     return dp[n];
-   	// }
 };
