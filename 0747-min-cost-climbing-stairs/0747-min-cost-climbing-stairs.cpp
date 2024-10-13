4b20f10c2e7b03f08594e85939bea8b1a75573f3
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-10-13 12:08:12 +0530

    Time: 0 ms (100%), Space: 16.8 MB (53.38%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,26 +1,41 @@
-class Solution {
-public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n=cost.size();
-        vector<int> dp(n+1,-1);
-        return min(mincost(cost,n-1,dp),mincost(cost,n-2,dp));
-    }
-    int mincost(vector<int>& cost, int n, vector<int>& dp){
-        //pruning
-        if(n<0){
-            return 0;
+class Solution
+{
+    public:
+        int minCostClimbingStairs(vector<int> &cost)
+        {
+            int n = cost.size();
+            if (n < 2)
+            {
+                return n == 0 ? 0 : cost[0];
+            }
+
+            vector<int> dp(n);
+            dp[0] = cost[0];
+            dp[1] = cost[1];
+
+            for (int i = 2; i < n; i++)
+            {
+                dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+            }
+
+            return min(dp[n - 1], dp[n - 2]);
         }
-        //base case
-        if(n==0||n==1){
-            return cost[n];
-        }
-        //cache
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        //save and compute
-        dp[n]=cost[n]+min(mincost(cost,n-1,dp),mincost(cost,n-2,dp));
-        //return
-        return dp[n];
-    }
+   	// int mincost(vector<int>& cost, int n, vector<int>& dp){
+   	//    	//pruning
+   	//     if(n < 0){
+   	//         return 0;
+   	//     }
+   	//    	//base case
+   	//     if(n==0||n==1){
+   	//         return cost[n];
+   	//     }
+   	//    	//cache
+   	//     if(dp[n]!=-1){
+   	//         return dp[n];
+   	//     }
+   	//    	//save and compute
+   	//     dp[n]=cost[n]+min(mincost(cost,n-1,dp),mincost(cost,n-2,dp));
+   	//    	//return
+   	//     return dp[n];
+   	// }
 };
