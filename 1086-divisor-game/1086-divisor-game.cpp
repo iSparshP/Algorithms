d53f8339e508abbb7020fb05ca2607bf6cd444c3
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-10-13 12:20:05 +0530

    Time: 2 ms (59.98%), Space: 7.4 MB (61.34%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.cpp
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    bool func(int num, vector<int>&dp){
-        if(num==1){
-            return 0;
-        }
-        if(dp[num]!=-1){
-            return dp[num];
-        }
-        int ans=0;
-        for(int i=1;i*i<num;i++){
-            if(num%i==0){
-                if(!func(num-i,dp)){
-                    ans=1;
-                    break;
-                }
-            }
-        }
-        return dp[num]=ans;
-    }
+    // bool func(int num, vector<int>&dp){
+    //     if(num==1){
+    //         return 0;
+    //     }
+    //     if(dp[num]!=-1){
+    //         return dp[num];
+    //     }
+    //     int ans=0;
+    //     for(int i=1;i*i<num;i++){
+    //         if(num%i==0){
+    //             if(!func(num-i,dp)){
+    //                 ans=1;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return dp[num]=ans;
+    // }
     bool divisorGame(int n) {
-        vector<int>dp(n+1,-1);
-        return func(n,dp);
+        // vector<int>dp(n+1,-1);
+        // return func(n,dp);
+        return (n%2==0);
     }
 };
