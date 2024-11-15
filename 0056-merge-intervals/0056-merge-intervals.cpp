8419f22aaa5dc6c953cb6281c030ef56e400d291
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-11-15 19:58:38 +0530

    Time: 3 ms (91.73%), Space: 22.8 MB (88.88%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.empty()){
+            return {};
+        }
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> result;
+        for(auto& interval:intervals){
+            if(result.empty() || result.back()[1]<interval[0]){
+                result.push_back(interval);
+            }
+            else{
+                result.back()[1]=max(result.back()[1],interval[1]);
+            }
+        }
+        return result;
+    }
+};
