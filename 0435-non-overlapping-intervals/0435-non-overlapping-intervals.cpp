993ed8ace317f5eb63ba9d0e36e807e27f218452
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-11-17 16:59:54 +0530

    Time: 46 ms (88.1%), Space: 93.7 MB (49.77%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if(intervals.empty())return 0;
         int count=0;
-        sort(intervals.begin(),intervals.end(),[](const vector<int>& a,const vector<int>& b){
-            return a[1]<b[1];
-        });
-        int end=INT_MIN;
-        for(auto& interval:intervals){
-            if(interval[0]>=end){
-                end=interval[1];
-            }
-            else{
+        sort(intervals.begin(),intervals.end(),[](vector<int>& a,vector<int>& b){return a[1]<b[1];});
+        int curr_end=INT_MIN;
+        for(const auto& interval:intervals){
+            if(interval[0]>=curr_end){
                 count++;
+                curr_end=interval[1];
             }
         }
-        return count;
-
+        return intervals.size()-count;
     }
 };
