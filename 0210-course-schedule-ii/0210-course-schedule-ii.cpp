ed6710db56ffd65aebcc1db8ec0ff87b894217ad
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-11-25 16:12:48 +0530

    Time: 3 ms (76.48%), Space: 17.6 MB (67.21%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int N=numCourses;
+        vector<int>adj[N];
+        for(auto it:prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        
+
+        queue<int> q; 
+	    vector<int> indegree(N, 0); 
+	    for(int i = 0;i<N;i++) {
+	        for(auto it: adj[i]) {
+	            indegree[it]++; 
+	        }
+	    }
+	    
+	    for(int i = 0;i<N;i++) {
+	        if(indegree[i] == 0) {
+	            q.push(i); 
+	        }
+	    }
+	    vector<int> topo;
+	    while(!q.empty()) {
+	        int node = q.front(); 
+	        q.pop(); 
+	        topo.push_back(node);
+	        for(auto it : adj[node]) {
+	            indegree[it]--;
+	            if(indegree[it] == 0) {
+	                q.push(it); 
+	            }
+	        }
+	    }
+	    if(topo.size()==N)return topo;
+        return {};
+    }
+};
