62a0e487b4587f01679c3a684ae379f08439cd58
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-12-01 16:59:49 +0530

    Time: 0 ms (100%), Space: 17.5 MB (5.25%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -1,7 +1,6 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         seen = {}
-
         for num in arr:
             if(2*num in seen) or (num%2==0 and num//2 in seen):
                 return True
