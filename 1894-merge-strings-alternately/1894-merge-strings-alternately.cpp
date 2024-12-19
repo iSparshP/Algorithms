8a99072616bf37502af0880b16caf889f7b33745
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-12-19 21:48:07 +0530

    Time: 0 ms (100%), Space: 8.4 MB (34.55%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string result="";
-        int m=max(word1.length(),word2.length());
-        for(int i=0;i<m;i++){
-            if(i<word1.length()){
-                result+=word1[i];
+        string ans="";
+        int n=max(word1.size(),word2.size());
+        for(int i=0;i<n;i++){
+            if(i<word1.size()){
+                ans+=word1[i];
             }
-            if(i<word2.length()){
-                result+=word2[i];
+            if(i<word2.size()){
+                ans+=word2[i];
             }
         }
-        return result;
+        return ans;
     }
 };
