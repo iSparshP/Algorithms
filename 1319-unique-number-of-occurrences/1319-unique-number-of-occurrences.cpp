9357ccd7da007b54f4c8d9ea42129511772a40c7
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-12-24 14:00:24 +0530

    Time: 0 ms (100%), Space: 12 MB (27.67%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int>mp;
+        for(int i:arr)mp[i]++;
         unordered_set<int>st;
-        for(auto i:arr)mp[i]++;
-        for(auto i:mp)st.insert(i.second);
+        for(auto itr:mp){
+            st.insert(itr.second);
+        }
         return mp.size()==st.size();
     }
 };
