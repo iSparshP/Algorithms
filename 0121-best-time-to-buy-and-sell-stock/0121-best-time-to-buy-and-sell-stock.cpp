7ac9c9264d72e8b80549779282a09aa3ccc9ebfd
Sparsh <77487266+iSparshP@users.noreply.github.com>
2025-02-24 21:43:32 +0530

    Time: 0 ms (100%), Space: 97.5 MB (7.09%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,8 +3,8 @@ public:
     int maxProfit(vector<int>& prices) {
         int minPrice=INT_MAX;
         int maxPrice=0;
-        for(auto &price:prices){
-            minPrice=min(minPrice,price);
+        for(auto & price:prices){
+            minPrice=min(price,minPrice);
             maxPrice=max(maxPrice,price-minPrice);
         }
         return maxPrice;
