2b490173728f24f3acb20db1906545ed611154a4
Sparsh <77487266+iSparshP@users.noreply.github.com>
2025-02-25 19:27:28 +0530

    Time: 0 ms (100%), Space: 8.2 MB (47.53%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,8 +3,10 @@ public:
     int hammingWeight(int n) {
         int count=0;
         while(n){
-            if(n&1)count++;
-            n>>=1;
+            if(n&1){
+                count++;
+            }
+            n=n>>1;
         }
         return count;
     }
