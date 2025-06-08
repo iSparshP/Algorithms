4bfaf233a5d7f98c4267eaccb661a02a76a4a588
Sparsh <77487266+iSparshP@users.noreply.github.com>
2025-06-08 10:56:38 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,25 +1,29 @@
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n<=2) return n;
+//         vector<int> dp(n+1);
+//         dp[1]=1;
+//         dp[2]=2;
+//         for(int i=3;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// };
+
+
+
 class Solution {
 public:
-    int solve(int n,vector<int>& dp){
-        //pruning
-        if(n<0){
-            return 0;
-        }
-        //base
-        if(n==0){
-            return 1;
-        }
-        //cache
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        //save and compute
-        dp[n]= solve(n-1,dp)+solve(n-2,dp);
-        //return 
-        return dp[n];
-    }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        if(n<=2) return n;
+        int prev2=1, prev1=2;
+        for(int i=3;i<=n;i++){
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
     }
 };
