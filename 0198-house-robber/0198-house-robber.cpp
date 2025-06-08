d00da8558dbe0c3865288a2613fe316a4eb94460
Sparsh <77487266+iSparshP@users.noreply.github.com>
2025-06-08 11:56:19 +0530

    Time: 0 ms (100%), Space: 10.6 MB (63.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    int dp[101];
-    int solve(vector<int>& nums, int n, int i){
-        //base
-        if(i>=n)
-        {
-            return 0;
-        }
-        //cache
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        //compute and save
-        int take=nums[i]+solve(nums,n,i+2);
-        int notTake=solve(nums,n,i+1);
-        return dp[i]=max(take,notTake);
-    }
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,nums.size(),0);
+        int n=nums.size();
+        if (n==1) return nums[0];
+        vector<int>dp(n);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
     }
 };
