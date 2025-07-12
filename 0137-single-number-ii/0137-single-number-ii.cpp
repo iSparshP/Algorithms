43ef946279dd5a659be7868038f890cac8972639
Sparsh <77487266+iSparshP@users.noreply.github.com>
2025-07-12 16:00:59 +0530

    Time: 0 ms (100%), Space: 13.4 MB (64.47%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int i=0;i<32;i++){
-            int sum=0;
-            for(auto num:nums){
-                sum+=((num>>i)&1);
-            }
-            sum %= 3;
-            ans |= sum << i;
+        int ones=0;
+        int twos=0;
+        for(const int num : nums){
+            ones ^= (num& ~twos);
+            twos ^= (num& ~ones);
         }
-        return ans;
+        return ones;
     }
 };
