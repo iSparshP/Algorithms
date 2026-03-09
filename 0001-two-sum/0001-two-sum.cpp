b33562c2dc3da63b8533ca1e773891c59504de48
Sparsh <77487266+iSparshP@users.noreply.github.com>
2026-03-09 22:11:37 +0530

    Time: 0 ms (100%), Space: 14.8 MB (52.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>nums_to_index_map;
-        for(int i=0;i<nums.size();i++){
-            int compliment=target-nums[i];
-            if(nums_to_index_map.count(compliment)){
-                return {nums_to_index_map[compliment],i};
+        unordered_map<int, int> nums_to_index_map;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int compliment = target - nums[i];
+            
+            if (nums_to_index_map.count(compliment)) {
+                return {nums_to_index_map[compliment], i};
             }
-            nums_to_index_map[nums[i]]=i;
+
+            nums_to_index_map[nums[i]] = i;
         }
+        
         return {};
     }
 };
