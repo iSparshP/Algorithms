5f7514a1034b332581a04a5390c379993357ea93
Sparsh <77487266+iSparshP@users.noreply.github.com>
2026-03-12 23:07:49 +0530

    Time: 940 ms (7.03%), Space: 11.6 MB (83.74%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
+    int recursion(int idx, vector<int>& nums, int target){
+        //Base
+        if(idx==nums.size()) return target ==0;
+        //Recursion
+        int plus = recursion(idx+1,nums,target-nums[idx]);
+
+        int minus = recursion(idx+1,nums,target+nums[idx]);
+
+        return plus + minus;
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int totalSum = accumulate(nums.begin(), nums.end(), 0);
-        vector<vector<int>> dp(nums.size(), vector<int>(2 * totalSum + 1, 0));
-
-        // Initialize the first row of the DP table
-        dp[0][nums[0] + totalSum] = 1;
-        dp[0][-nums[0] + totalSum] += 1;
-
-        // Fill the DP table
-        for (int index = 1; index < nums.size(); index++) {
-            for (int sum = -totalSum; sum <= totalSum; sum++) {
-                if (dp[index - 1][sum + totalSum] > 0) {
-                    dp[index][sum + nums[index] + totalSum] +=
-                        dp[index - 1][sum + totalSum];
-                    dp[index][sum - nums[index] + totalSum] +=
-                        dp[index - 1][sum + totalSum];
-                }
-            }
-        }
-
-        // Return the result if the target is within the valid range
-        return abs(target) > totalSum ? 0
-                                      : dp[nums.size() - 1][target + totalSum];
+        return recursion(0, nums, target);
     }
 };
